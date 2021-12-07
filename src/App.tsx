877b966027ca543bd60d27a7d8f58abde980efff
YonneLee <751144428@qq.com>
2021-12-07 17:21:37 +0800

    测试

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import "./App.css";
 
 function App() {
   const { user } = useAuth();
-  console.log(user)
   return (
     <div className="App">
       {user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}
